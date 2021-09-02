06fc9d8cd02cfcad2971896f40f47296d273509f
top1st <riguang102@gmail.com>
2021-09-02 23:02:56 +0800

    Router Address & favicon & sdk + uikit

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap/sdk'
 import { BUSD, DAI, USDT, BTCB, CAKE, WBNB, UST, ETH, USDC } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0xE09E30b8a57b245Ba48595663204f5dbAB75bced'
 
 // a list of tokens by chain
 type ChainTokenList = {
